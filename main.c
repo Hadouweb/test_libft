3204bc9c7e9c6361ab60ece4f75cbd0add50ec84
Hadouweb <kengo14@hotmail.fr>
2016-11-20 06:29:21 +0100

    Yessss

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include "libft.h"
 
 int main()
 {
 	char byte;
-	int fd = open("/dev/ttys2", O_RDWR);
+	int fd = open("/dev/ttys001", O_RDWR);
 	dprintf(fd, "dsfkjsdhfjkdshfks\n");
+	ft_putstr("lol \n");
+
 	return 0;
 }
